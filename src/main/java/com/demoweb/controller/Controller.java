415013e9fc8a3607c019c327ba0ed7c2e1955f3c
Natalia Monge <nm.mnavarro@gmail.com>
2020-10-22 10:52:07 -0300

    Services

--- src/main/java/com/demoweb/controller/Controller.java
+++ src/main/java/com/demoweb/controller/Controller.java
@@ -1,18 +1,24 @@
 package com.demoweb.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.demoweb.model.Admin;
+import com.demoweb.model.Client;
 import com.demoweb.modelinterface.IAdmin;
+import com.demoweb.modelinterface.IClient;
+import com.demoweb.serviceinterfaces.IClientService;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
 	@Autowired
 	private IAdmin adminrepo;
+
 	
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name",required=false,defaultValue="World")String name, Model model) {
@@ -25,6 +31,14 @@ public class Controller {
 		
 		model.addAttribute("name",name);
 		return "greeting";
-	}
+	}/*
+	@GetMapping("/clientread")
+	public String greeting(Model model) {
+		
+		
+		List<Client> clients = clientservice.read();
+		model.addAttribute("clients",clients);
+		return "clientread";
+	}*/
 }
  
--- src/main/java/com/demoweb/model/Client.java
+++ src/main/java/com/demoweb/model/Client.java
@@ -22,4 +22,28 @@ private String email;
 private String phone;
 @Column(name="name")
 private String name;
+public int getId_administrator() {
+	return id_administrator;
+}
+public void setId_administrator(int id_administrator) {
+	this.id_administrator = id_administrator;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPhone() {
+	return phone;
+}
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
 }
